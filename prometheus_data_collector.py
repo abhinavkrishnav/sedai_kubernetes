785b535cac03101f6beb038307fc1254e09f8d0a
abhinavkrishnav <abhinavkrishnav6238@gmail.com>
2024-10-31 14:34:46 +0530

    Cpu metrics and Memory Usage

--- prometheus_data_collector.py
+++ prometheus_data_collector.py
@@ -1,9 +1,8 @@
 import requests
-import datetime
+import json
 
-PROMETHEUS_URL = "http://34.66.104.228:80/api/v1/query"  # Replace with actual host
+PROMETHEUS_URL = "http://34.66.104.228:80/api/v1/query"
 
-# Example function to fetch CPU usage of nodes
 def fetch_metric(query):
     response = requests.get(PROMETHEUS_URL, params={'query': query})
     if response.status_code == 200:
@@ -13,12 +12,34 @@ def fetch_metric(query):
         print(f"Error fetching data: {response.status_code}")
         return None
 
-# Define your PromQL query
 cpu_query = 'rate(node_cpu_seconds_total[5m])'
+memory_total_query = 'node_memory_MemTotal_bytes'
+memory_available_query = 'node_memory_MemAvailable_bytes'
 
-# Collect data
-metrics = fetch_metric(cpu_query)
-for metric in metrics:
-    print(f"Metric: {metric['metric']}")
-    print(f"Values: {metric['value']}")
+print("CPU Metrics: ")
+cpu_metrics = fetch_metric(cpu_query)
+if cpu_metrics:
+   for metric in cpu_metrics:
+      print("\nMetric Information:")
+      print(json.dumps(metric['metric'],indent=4))
+      print("values:")
+      print(f"  Timestamp: {metric['value'][0]}")
+      print(f"  CPU Usage: {metric['value'][1]}")
+else:
+    print("No CPU metrics data retrieved.")
+
+print("\nMemory Metrics")
+memory_total = fetch_metric(memory_total_query)
+memory_available = fetch_metric(memory_available_query)
+
+if memory_total and memory_available:
+    for total, available in zip(memory_total, memory_available):
+        print("\nNode:", total['metric']['node'])
+        print("Memory Total:", total['value'][1])
+        print("Memory Available:", available['value'][1])
+        used_memory = float(total['value'][1]) - float(available['value'][1])
+        print("Memory Used:", used_memory)
+else:
+    print("No memory metrics data retrieved.")
+
 
